d476ad02afe722f4f18e43a6fa50b06a72ff86fa
Tresor-ilunga <19im065@esisalama.org>
2022-05-04 23:34:17 +0200

    Gestion d'un formulaire

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require('nodemailer');
 const router = express.Router();
 
 /* GET home page*/
@@ -9,6 +10,44 @@ router.get('/', (req, res)=>{
 /* GET contact page */
 router.get('/contact', (req, res)=>{
     res.render('contact', {title: "Contact Page"});
+})
+
+router.post('/contact', (req,res)=>{
+    // récupération des données du formulaire
+    //console.log(req.body);
+
+
+    // envoie du mail
+    let transpoter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'ti6177854@gmail.com',
+            pass: 'Hello@4166'
+        }
+    });
+
+    let mailOptions ={
+        from: req.body.email,
+        to: 'ti6177854@gmail.com',
+        subject: req.body.subject,
+        text: req.body.message
+    };
+
+    transpoter.sendMail(mailOptions, (err,infos)=>{
+        if(err)
+        {
+            console.log(err);
+            res.render('contact',{
+                title: "Contact Page",
+                error: "Désolé, votre message n'as pas été envoyé."});
+        }else
+        {
+            console.log(infos);
+            res.render('contact',{
+                title: "Contact Page", 
+                success: "Votre message à été envoyé avec succès."});
+        }
+    })
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const { Router } = require('express');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
 const indexRouter = require('./routes/index.route');
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.set("views", "templates");
 app.set("view engine", "twig");
